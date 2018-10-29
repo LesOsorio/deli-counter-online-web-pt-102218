156017a5006a55abd0cd77906cc522c2818c2f09
Leslie Osorio <lesosorio17@gmail.com>
2018-10-29 19:55:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,14 +9,19 @@ katz_deli = []
     puts "The line is empty"
 
   def now_serving(katz_deli)
-    
+    puts "The line is empty."
 
   def line(katz_deli)
+    line = ["Ada"]
+    puts "There are #{num} people in line."
+  end 
 
   def take_a_number(katz_deli, "Matz")
+    line = ["Ada"] <<"Matz"
+  end
 
   def line(katz_deli)
 
   def now_serving(katz_deli)
 
-  def line(katz_deli) 
+  def line(katz_deli)
