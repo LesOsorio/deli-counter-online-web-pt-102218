28b0c9fe2427b3a167037ee8229cebf8d3560b56
Leslie Osorio <lesosorio17@gmail.com>
2018-10-29 19:47:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def katz_deli(line)
+  line = [ ]
+end 
+
+def other_deli(line)
+  line = [ ]
+end 
+
+def take_a_number(line)
+  line = ["Ada"]
+end 
+
+def take_a_number(line)
+  line = ["Ada"] <<"Grace"
+end 
