3cad73d845bdbc49168aa6450d661ba180360e5d
Leslie Osorio <lesosorio17@gmail.com>
2018-10-29 19:50:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,19 @@
 katz_deli = []
 
-  take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
-  take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
-  take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+  take_a_number(katz_deli, "Ada") 
+  take_a_number(katz_deli, "Grace") 
+  take_a_number(katz_deli, "Kent") 
 
-  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+  line(katz_deli) 
 
-  now_serving(katz_deli) #=> "Currently serving Ada."
+  now_serving(katz_deli) 
 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+  line(katz_deli) 
+  
+  take_a_number(katz_deli, "Matz") 
 
-  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
+  line(katz_deli) 
+  
+  now_serving(katz_deli) 
 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
-  now_serving(katz_deli) #=> "Currently serving Grace."
-
-  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
+  line(katz_deli) 
