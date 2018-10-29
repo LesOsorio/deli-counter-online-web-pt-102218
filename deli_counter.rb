af79458ea1bbc814c89940ec397b525b687c957e
Leslie Osorio <lesosorio17@gmail.com>
2018-10-29 19:50:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,19 @@
-def katz_deli(line)
-  line = []
-  puts "The line is empty."
-end
+katz_deli = []
 
-def other_deli(line)
-  line = ["leslie","peter"]
-end
+  take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+  take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
+  take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
 
-def take_a_number(line)
-  line = ["Ada"]
-end
+  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-def take_a_number(line)
-  line = ["Ada"] <<"Grace"
-end
+  now_serving(katz_deli) #=> "Currently serving Ada."
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
+
+  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+  now_serving(katz_deli) #=> "Currently serving Grace."
+
+  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
